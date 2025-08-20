b5945750b6c55737aa68c972d62dbd32e8e8f9a3
Aparna Vinod <aparnavinod083@gmail.com>
2025-08-20 11:20:55 +0530

    updated

--- src/Service/commonAPI.js
+++ src/Service/commonAPI.js
@@ -1,17 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const commonAPI = async(httpRequest,url,reqBody,reqHeader)=>{
-    const reqConfig = {
-        method:httpRequest,
-        url,
-        data:reqBody,
-        headers:reqHeader?reqHeader:{"Content-Type":"application/json"}
+export const commonAPI = async (httpRequest, url, reqBody, reqHeader) => {
+  const reqConfig = {
+    method: httpRequest,
+    url,
+    data: reqBody,
+    headers: reqHeader || { "Content-Type": "application/json" },
+    withCredentials: true
+  };
 
-    }
-    return await axios(reqConfig).then(res=>{
-        return res
-
-    }).catch(err=>{
-        return err
-    })
-}
+  return await axios(reqConfig); 
+};
